a775efa2af35b5a43b02ff173396703ff5cf5de9
Allan <allansraj@gmail.com>
2020-02-27 21:39:32 -0500

    Homework6 - Reach

--- myproject.py
+++ myproject.py
@@ -15,3 +15,26 @@ printme ("My Projectdataset", iris[:])
 #printme ("My Projectdataset", iris.columns)
 printme("correlation between fields", iris.corr().to_string())
 
+# Plotting histogram
+plt.hist(iris['sepal length in cm'])
+plt.title('sepal length in cm')
+plt.xlabel('sepal length in cm')
+plt.ylabel('sepal length in cm')
+plt.savefig(f'plots/sepal length in cm.png', format='png')
+plt.clf()
+
+# Plotting scatterplot
+plt.scatter(iris['sepal length in cm'], iris['petal length in cm'], color='b')
+plt.title('sepal to petal')
+plt.xlabel('sepal length in cm')
+plt.ylabel('petal length in cm')
+plt.savefig(f'plots/sepal_to_petal_scatter.png', format='png')
+plt.clf()
+
+# Plotting line chart
+plt.plot(iris['petal width in cm'], color='red')
+plt.title('petal width in cm')
+plt.xlabel('Petal width')
+plt.ylabel('Petal Width')
+plt.savefig(f'plots/petal width in cm.png', format='png')
+plt.close()
